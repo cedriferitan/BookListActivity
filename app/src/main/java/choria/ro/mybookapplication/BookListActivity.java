5467b3eb63bd85b5fee55497ce1a4a7621570945
Cedrik Cane <cedriferitan@gmail.com>
2019-01-07 18:44:56 +0200

    adapter + model + manifest addings

--- app/src/main/java/choria/ro/mybookapplication/BookListActivity.java
+++ app/src/main/java/choria/ro/mybookapplication/BookListActivity.java
@@ -1,13 +1,71 @@
 package choria.ro.mybookapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import choria.ro.mybookapplication.adapter.BookAdapter;
+import choria.ro.mybookapplication.model.Book;
+import choria.ro.mybookapplication.rest.client.BookClient;
+import cz.msebera.android.httpclient.Header;
 
 public class BookListActivity extends AppCompatActivity {
+    public static final String BOOK_DETAIL_KEY = "book";
+    private ProgressBar progress;
+
+    private ListView lvBooks;
+    private BookAdapter bookAdapter;
+    private BookClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
+        //
+        lvBooks = findViewById(R.id.lvBooks);
+        ArrayList<Book> aBooks = new ArrayList<Book>();
+        bookAdapter = new BookAdapter(this, aBooks);
+        lvBooks.setAdapter(bookAdapter);
+        //
+        fetchBooks();
+    }
+
+    // Executes an API call to the OpenLibrary search endpoint, parses the results
+    // Converts them into an array of book objects and adds them to the adapter
+    private void fetchBooks() {
+        client = new BookClient();
+        client.getBooks("Jordan peterson", new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONArray docs;
+                    if(response != null) {
+                        // Get the docs json array
+                        docs = response.getJSONArray("docs");
+                        // Parse json array into array of model objects
+                        final ArrayList<Book> books = Book.fromJson(docs);
+                        // Remove all books from the adapter
+                        bookAdapter.clear();
+                        // Load model objects into the adapter
+                        for (Book book : books) {
+                            bookAdapter.add(book); // add book through the adapter
+                        }
+                        bookAdapter.notifyDataSetChanged();
+                    }
+                } catch (JSONException e) {
+                    // Invalid JSON format, show appropriate error.
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
